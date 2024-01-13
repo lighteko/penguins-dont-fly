a1d109a9c52d68a7768089169b50e64d3f564550
lighteko <huijunyi0212@naver.com>
2024-01-13 22:48:32 +0900

    ..

--- Assets/Scripts/Penguin.cs
+++ Assets/Scripts/Penguin.cs
@@ -1,21 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 public class Penguin : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P)) {
-            Vector2 forceDirection = new Vector2(5,4);
-            GetComponent<Rigidbody2D>().AddForce(forceDirection * 5f);
-        }
+
     }
+
+    void FixedUpdate() {
+        
+    }
+
 }
